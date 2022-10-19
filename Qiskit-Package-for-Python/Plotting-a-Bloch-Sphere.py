a645caa3cc5b77720679670dba2c7e50ef1d9050
Mischka-Plyushevyy <116179142+Mischka-Plyushevyy@users.noreply.github.com>
2022-10-19 19:39:54 +0100

    Update Plotting-a-Bloch-Sphere.py
    Insert comments

--- Qiskit-Package-for-Python/Plotting-a-Bloch-Sphere.py
+++ Qiskit-Package-for-Python/Plotting-a-Bloch-Sphere.py
@@ -1,15 +1,20 @@
+#import the necessary items
 from qiskit import *
 from qiskit.tools.visualization import *
 from qiskit.visualization import *
 
+#generate a quantum circuit with 2 entangled qubits 
 circuit = QuantumCircuit(2,2)
 circuit.h(0)
 circuit.cx(0,1)
+
+#measure the entangled pair on a simulator and visualise results 
 circuit.measure([0,1],[0,1])
 simulator = Aer.get_backend('qasm_simulator')
 result = execute(circuit, simulator).result()
 plot_histogram(result.get_counts(circuit))
 
+#simulate the Bloch sphere of the qubits for visualisation of their states 
 backend = Aer.get_backend('statevector_simulator')
 result = execute(circuit, backend).result()
 final_state = result.get_statevector()
