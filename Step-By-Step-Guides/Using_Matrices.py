c2aebf47c26642667f26a8ff12a4748dc5425979
Sai Ganesh Manda <89340753+mvsg2@users.noreply.github.com>
2022-10-19 23:14:52 +0530

    Update Using_Matrices.py

--- Step-By-Step-Guides/Using_Matrices.py
+++ Step-By-Step-Guides/Using_Matrices.py
@@ -1,5 +1,7 @@
 import numpy as np
 # import time
+
+# Defining a function that adds two matrices
 def MatrixAddition(matrix_a, matrix_b):
     m,n = matrix_a.shape
     matrix_c = [[0 for i in range(m)] for j in range(n)]
@@ -8,6 +10,7 @@ def MatrixAddition(matrix_a, matrix_b):
             matrix_c[i][j] += matrix_a[i][j] + matrix_b[i][j]
     return np.array(matrix_c)
 
+# Defining a function that multiplies two matrices 
 def MatrixMultiplication(matrix_a, matrix_b):
     m,n = matrix_a.shape
     n,p= matrix_b.shape
@@ -18,15 +21,22 @@ def MatrixMultiplication(matrix_a, matrix_b):
                 matrix_c[i][j] += matrix_a[i][k]*matrix_b[k][j]
     return np.array(matrix_c)
 
+# Taking the shape-input from user...
 a = int(input("Enter how many rows: "))
 b = int(input("Enter how many columns: "))
+
 print("\n")
+
+# Asking the user to initializa the elements of the matrices...
+print("Enter the elements of the first matrix please")
 first_matrix = [[0 for i in range(a)] for j in range(b)]
 for i in range(a):
     for j in range(b):
         first_matrix[i][j] += int(input("first_matrix["+str(i)+"]["+str(j)+"]: "))
 first_matrix = np.array(first_matrix)
-print("\n")
+
+print("Now enter the elements of the second matrix please")
+
 second_matrix = [[0 for i in range(a)] for j in range(b)]
 for i in range(a):
     for j in range(b):
