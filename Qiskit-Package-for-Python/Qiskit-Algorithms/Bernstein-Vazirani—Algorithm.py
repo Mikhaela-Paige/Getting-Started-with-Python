38d0ef5fc3e3a320c8ba8c48e8931c28fca1db8e
Mischka-Plyushevyy <116179142+Mischka-Plyushevyy@users.noreply.github.com>
2022-10-20 08:36:09 +0100

    Update Bernstein-Vazirani—Algorithm.py
    Added comments

--- Qiskit-Package-for-Python/Qiskit-Algorithms/Bernstein-Vazirani—Algorithm.py
+++ Qiskit-Package-for-Python/Qiskit-Algorithms/Bernstein-Vazirani—Algorithm.py
@@ -4,15 +4,15 @@ from qiskit.tools.visualization plot_histogram
 #to generalise for any given secretnumber
 secretnumber = '#number'
 
-#
-circuit = QuantumCircuit(len(secretnumber)+1,len(secretnumber))
-circuit.h(range(len(secretnumber)))
-circuit.x(len(secretnumber))
-circuit.h(len(secretnumber))
+#circuit set-up
+circuit = QuantumCircuit(len(secretnumber)+1,len(secretnumber)) #(The no. of bits in secretnumber + 1, the no. of bits in secretnumber)
+circuit.h(range(len(secretnumber))) #[all of the bits in the secret number]
+circuit.x(len(secretnumber))#[the + 1 bit]
+circuit.h(len(secretnumber))#[the + 1 bit]
 circuit.barrier()
 circuit.draw(output = 'mpl')
 
-#
+#to find (bit that is equal to one, the + 1 bit)
 for ii, yesno in enumerate(reversed(secretnumber)):
   if yesno == '1':
     circuit.cx(ii, len(secretnumber))
@@ -21,19 +21,17 @@ circuit.barrier() #all barrier insertions are for visual purposes for the .draw 
 circuit.draw(output = 'mpl')
 
 #
-circuit.h(range(len(secretnumber)))
+circuit.h(range(len(secretnumber))) #[all of the bits in the secret number]
 circuit.barrier()
 circuit.draw(output = 'mpl')
 
-#
-circuit.measure(range(len(secretnumber)), range(len(secretnumber)))
+circuit.measure(range(len(secretnumber)), range(len(secretnumber)))  #[all of the bits in the secret number], [classical bit for all of the bits in the secret number]
 circuit.draw(output = 'mpl')
 
-#
 simulator = Aer.get_backend('qasm_simulator')
 result = execute(circuit, backend = simulator, shots = 1).result()
 counts = result.get_counts()
-print(counts)
+print(counts) #outputs the secretnumber
 
 #to compare calculated number with secretnumber
 print(secretnumber)
